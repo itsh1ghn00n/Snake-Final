ee8726000c33cb23e04a8bda0f793653030cde54
itsh1ghn00n <nico.castaldo@gmail.com>
2025-10-20 16:56:31 -0600

    init

--- main.cpp
+++ main.cpp
@@ -13,13 +13,16 @@ void fillBoard(char board[row][col]) {
         }
     }
 }
+
 void printBoard(char board[row][col]) {
+    // Column Lettering = #Col
     char letterVal = 'A';
     cout << " "; //This sets the spacing to the correct size
     for (int i = 0; i < col; i++) {
         cout << '[' << letterVal << ']';
         letterVal += 1;
     }
+    // Row Numbering = #Row
     for (int i = 0; i < row; i++) {
         cout << '\n' << i + 1;
         for (int j = 0; j < col; j++) {
@@ -27,29 +30,30 @@ void printBoard(char board[row][col]) {
         }   // This loop is responsible for printing the value that is currently inside of the boards.
     }   // This loop is used to indent the values
 }
-bool checkApple(char board[row][col]) {
+
+bool checkApple(int row, int col) {
     if (board[row][col] == '*') {
         return true;
+        // Apple here
+    } else {
+        return false;
+        // No Apple
     }
 }
+
 int main() {
     char gameBoard[row][col];
     fillBoard(gameBoard);
     printBoard(gameBoard);
-    bool winCond = false;
+    bool winCon = false;
     int playerLen = 1;
     int playerXval = 0, playerYval = 0; //this is setting the Y and X cords for the game board
     char playerLastInput = 'd'; //this will keep track of players previous input starting going right
 
-    while (winCond == false) {
-
-
-
-
-
-
+    while (winCon == false) {
+        // wincon
         if (row * col == playerLen) {
-            winCond = true;
+            winCon = true;
         }
     }
     return 0;

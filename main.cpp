361fffb178ac436119ec4c2b5862f98d8c9f8cd6
Andrew Hirt <142833305+WowlSowl@users.noreply.github.com>
2025-10-20 17:07:41 -0600

    some stuff

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@ using namespace std;
 //this will be the main runner for the game
 
 constexpr int row = 7; //n-1 so this creates a game board of 5 but to access last slot it will be n-1
-constexpr int col = 15;
-int board[row][col] = {};
+constexpr int col = 10;
+char board[row][col] = {};
 
 void fillBoard(char board[row][col]) {
     for (int i = 0; i < row; i++) {
@@ -30,6 +30,7 @@ void printBoard(char board[row][col]) {
         }   // This loop is responsible for printing the value that is currently inside of the boards.
     }   // This loop is used to indent the values
 }
+<<<<<<< Updated upstream
 
 bool checkApple(int row, int col) {
     if (board[row][col] == '*') {
@@ -38,22 +39,47 @@ bool checkApple(int row, int col) {
     } else {
         return false;
         // No Apple
+=======
+bool checkApple(int row, int col) {
+    if (board[row][col] == '*') {
+        return true; //which means apple is currently at position
+>>>>>>> Stashed changes
     }
 }
 
 int main() {
+<<<<<<< Updated upstream
     char gameBoard[row][col];
     fillBoard(gameBoard);
     printBoard(gameBoard);
     bool winCon = false;
+=======
+    fillBoard(board);
+    printBoard(board);
+    bool winCond = false;
+>>>>>>> Stashed changes
     int playerLen = 1;
     int playerXval = 0, playerYval = 0; //this is setting the Y and X cords for the game board
     char playerLastInput = 'd'; //this will keep track of players previous input starting going right
+    int winLength = row * col;
 
+<<<<<<< Updated upstream
     while (winCon == false) {
         // wincon
         if (row * col == playerLen) {
             winCon = true;
+=======
+    while (winCond == false) {
+
+
+
+
+
+
+
+        if (winLength == playerLen) {
+            winCond = true;
+>>>>>>> Stashed changes
         }
     }
     return 0;

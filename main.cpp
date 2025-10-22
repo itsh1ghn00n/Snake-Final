9dcfd79a2243fba5abee5bf96b1f902de20bfd02
Andrew Hirt <142833305+WowlSowl@users.noreply.github.com>
2025-10-22 10:20:02 -0600

    added class for board

--- main.cpp
+++ main.cpp
@@ -4,17 +4,17 @@ using namespace std;
 
 constexpr int row = 7; //n-1 so this creates a game board of 5 but to access last slot it will be n-1
 constexpr int col = 10;
-char board[row][col] = {};
+int board[row][col] = {};
 
-void fillBoard(char board[row][col]) {
+void fillBoard(int board[row][col]) {
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
-            board[i][j] = '0';
+            board[i][j] = 1;
         }
     }
 }
 
-void printBoard(char board[row][col]) {
+void printBoard(int board[row][col]) {
     // Column Lettering = #Col
     char letterVal = 'A';
     cout << " "; //This sets the spacing to the correct size
@@ -30,7 +30,6 @@ void printBoard(char board[row][col]) {
         }   // This loop is responsible for printing the value that is currently inside of the boards.
     }   // This loop is used to indent the values
 }
-<<<<<<< Updated upstream
 
 bool checkApple(int row, int col) {
     if (board[row][col] == '*') {
@@ -39,37 +38,30 @@ bool checkApple(int row, int col) {
     } else {
         return false;
         // No Apple
-=======
 bool checkApple(int row, int col) {
     if (board[row][col] == '*') {
         return true; //which means apple is currently at position
->>>>>>> Stashed changes
     }
 }
 
 int main() {
-<<<<<<< Updated upstream
-    char gameBoard[row][col];
+    int gameBoard[row][col];
     fillBoard(gameBoard);
     printBoard(gameBoard);
     bool winCon = false;
-=======
     fillBoard(board);
     printBoard(board);
     bool winCond = false;
->>>>>>> Stashed changes
     int playerLen = 1;
     int playerXval = 0, playerYval = 0; //this is setting the Y and X cords for the game board
     char playerLastInput = 'd'; //this will keep track of players previous input starting going right
     int winLength = row * col;
 
-<<<<<<< Updated upstream
     while (winCon == false) {
         // wincon
         if (row * col == playerLen) {
             winCon = true;
-=======
-    while (winCond == false) {
+            while (winCond == false) {
 
 
 
@@ -77,10 +69,11 @@ int main() {
 
 
 
-        if (winLength == playerLen) {
-            winCond = true;
->>>>>>> Stashed changes
+                if (winLength == playerLen) {
+                    winCond = true;
+                }
+            }
+            return 0;
         }
     }
-    return 0;
 }
